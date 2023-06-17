b9fe31c1396994774ccfcd1100f0ca7fcef81d6e
Cristina McClain <cristinarmcclain@gmail.com>
2023-06-17 13:58:48 -0400

    first commit

---
+++ City.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    internal class City
+    {
+        public int ZipCode { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public int Population { get; set; }
+    }
+
+        internal class Apartment : Building
+        {
+            public int NumberOfUnits { get; set; }
+            public int NumberOfOpenUnits { get; set; }
+            public bool HasParking { get; set; }
+        }
+
+        internal class Building
+        {
+            public string Street { get; set; }
+
+            public int StreetNumber { get; set; }
+        }
+
+    internal class HighRise : Building
+    {
+        public int NumberOfStories { get; set; }
+        public string OwnerName { get; set; }
+        public List<string> Directory { get; set; }
+    }
+}
